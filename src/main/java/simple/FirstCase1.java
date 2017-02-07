aa04f4d1e61cac6cb6dbbfd84163e9faf14f5820
Kishore <Kishore>
2017-02-07 11:57:22 +0530

    commit1

---
+++ src/main/java/simple/FirstCase1.java
@@ -0,0 +1,14 @@
+package simple;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstCase1 {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.pfchangs.com");
+
+	}
+
+}
