5d807cfe8ead98f5deb0c2162414167f8ea49590
qatestt <qatesstt@gmail.com>
2017-02-08 14:00:59 +0530

    mastercm3

--- src/main/java/simple/FirstCase1.java
+++ src/main/java/simple/FirstCase1.java
@@ -8,14 +8,9 @@ public class FirstCase1 {
 	public static void main(String[] args) {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.pfchangs.com");
-		System.out.println("testing ");
 
-	System.out.println("im in master");
 
-		System.out.println("i have made changes in branch");
-		System.out.println("im done with new branch");
-
-System.out.println("new change in branch");
+	
 	}
 
 }
