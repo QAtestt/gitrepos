9279713e00eaef85ceab23696afa5476d61dc4ad
qatestt <qatesstt@gmail.com>
2017-02-08 14:32:54 +0530

    cm12

--- src/main/java/simple/FirstCase1.java
+++ src/main/java/simple/FirstCase1.java
@@ -8,12 +8,10 @@ public class FirstCase1 {
 	public static void main(String[] args) {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.pfchangs.com");
-<<<<<<< HEAD
+
 		System.out.println("testing ");
 		System.out.println("im making changes in teeebranch");
-=======
->>>>>>> 5d807cfe8ead98f5deb0c2162414167f8ea49590
-
+System.out.println("im in master");
 
 	
 	}
