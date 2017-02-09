8321cd487953fb9d7409cd9f841598a0e7fe2337
qatestt <qatesstt@gmail.com>
2017-02-09 12:04:07 +0530

    branch commiting

--- src/main/java/simple/FirstCase1.java
+++ src/main/java/simple/FirstCase1.java
@@ -11,17 +11,17 @@ public class FirstCase1 {
 
 		System.out.println("testing ");
 		System.out.println("im making changes in teeebranch");
-<<<<<<< HEAD
+
 
 		System.out.println("im going to make a false commit");
 		System.out.println("im re-do ing it");
 		System.out.println("im trying for 2ndd time");
-		
+		System.out.println("im making a change in testbranch");
 
 	
-=======
+
 System.out.println("im in master");
->>>>>>> 9279713e00eaef85ceab23696afa5476d61dc4ad
+
 
 	
 	}
