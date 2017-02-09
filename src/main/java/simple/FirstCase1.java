923f713ff99d045d86f0c7ff1a692ee843c5c159
qatestt <qatesstt@gmail.com>
2017-02-09 12:19:54 +0530

    commiting del branch

--- src/main/java/simple/FirstCase1.java
+++ src/main/java/simple/FirstCase1.java
@@ -9,27 +9,7 @@ public class FirstCase1 {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.pfchangs.com");
 
-		System.out.println("testing ");
-		System.out.println("im making changes in teeebranch");
-
-
-		System.out.println("im going to make a false commit");
-		System.out.println("im re-do ing it");
-		System.out.println("im trying for 2ndd time");
-<<<<<<< HEAD
-		System.out.println("im making a change in testbranch");
-
-	
-
-System.out.println("im in master");
-
-=======
-		system.out.println("merging conflict");
-		system.out.println("checkin");
-	
-
-System.out.println("im in master");
->>>>>>> a3583ebf265c1b7c181d913f198d6d6f7eda09f2
+		
 
 	
 	}
