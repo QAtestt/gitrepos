9663c62126dd2315c875d68ce0f0188dd228d732
qatestt <qatesstt@gmail.com>
2017-02-13 11:21:33 +0530

    mucommit

--- src/main/java/simple/FirstCase1.java
+++ src/main/java/simple/FirstCase1.java
@@ -9,15 +9,7 @@ public class FirstCase1 {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.pfchangs.com");
 
-		System.out.println("testing ");
-		System.out.println("im making changes in teeebranch");
-
-
-		System.out.println("im going to make a false commit");
-		System.out.println("im re-do ing it");
-		System.out.println("im trying for 2ndd time");
-		system.out.println("merging conflict");
-		system.out.println("checkin");
+		
 	
 
 System.out.println("im in master");
